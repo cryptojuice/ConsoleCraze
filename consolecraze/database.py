78c7cd103dabf91bfc15d9cfb03c90a631621146
Nick Robinson <grobins2@gmail.com>
2013-07-20 13:34:20 -0400

    renamed db variable to db_session

--- consolecraze/database.py
+++ consolecraze/database.py
@@ -1,16 +1,20 @@
-from config import SQLALCHEMY_DATABASE_URI
-
+import os
+from consolecraze import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, convert_unicode = True)
-db = scoped_session(sessionmaker(autocommit=False,
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode = True)
+db_session = scoped_session(sessionmaker(autocommit=False,
     autoflush=False,
     bind=engine))
 
 Base = declarative_base()
-Base.query = db.query_property()
+Base.query = db_session.query_property()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
+def drop_all():
+    Base.metadata.drop_all(bind=engine)
