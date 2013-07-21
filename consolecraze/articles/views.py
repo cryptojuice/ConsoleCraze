a5eba4f14f9e5cc96c6a31b756aaa8a3afd38c07
Nick Robinson <grobins2@gmail.com>
2013-07-21 15:52:38 -0400

    refactoring

--- consolecraze/articles/views.py
+++ consolecraze/articles/views.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
-from werkzeug import check_password_hash, generate_password_hash
+from flask import Blueprint, request, render_template, flash, g, session, \
+        redirect, url_for, jsonify
 
 from consolecraze.database import db_session
 from consolecraze.articles.models import Article
 from consolecraze.users.decorators import requires_login
 
-mod = Blueprint('articles', __name__)
+mod = Blueprint('articles', __name__, url_prefix='/articles')
+
+@mod.route("/")
+def index():
+    return jsonify(articles=[
+        {
+            "title":"Dead Rising 3 started on 360, pushed the hardware to far",
+            "summary":"Dead Rising 3 first surfaced almost two years ago. So how did it end up an Xbox One title? According to Capcom Vancouver producer Mike Jones, talking to Siliconera, it was a matter of resources.",
+            "url":"http://www.destructoid.com/dead-rising-3-started-on-360-pushed-the-hardware-too-far-258452.phtml",
+            "thumbnail_url":"",
+            "user_id": 0, 
+            "upvoted_by": None,
+            "downvoted_by": None
+        }
+    ])
