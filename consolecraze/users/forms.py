9f2deb68b5f34287f65f5690d92e229452007205
Nick Robinson <grobins2@gmail.com>
2013-07-22 12:09:58 -0400

    form update

--- consolecraze/users/forms.py
+++ consolecraze/users/forms.py
@@ -2,15 +2,14 @@ from wtforms import Form, TextField, PasswordField, BooleanField
 from wtforms.validators import Required, Email, EqualTo
 
 class LoginForm(Form):
-    email = TextField('Email Address', [Required(), Email()])
+    email = TextField('E-mail', [Required(), Email()])
     password = PasswordField('Password', [Required()])
 
 class RegisterForm(Form):
-    name = TextField('NickName', [Required()])
-    email = TextField('Email Address', [Required(), Email()])
+    name = TextField('Name', [Required()])
+    email = TextField('E-mail', [Required(), Email()])
     password = PasswordField('Password', [Required()])
     confirm = PasswordField('Repeat Password', [
         Required(),
         EqualTo('password', message='Passwords must match')
         ])
-    accept_tos = BooleanField('I accept the TOS')
