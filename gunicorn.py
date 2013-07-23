958a0ac1d3aef926fd30da4ecf10e5162192c00e
Nick Robinson <grobins2@gmail.com>
2013-07-23 01:30:43 -0400

    port update

--- gunicorn.py
+++ gunicorn.py
@@ -4,7 +4,7 @@ os.environ['DIAG_CONFIG_MODULE'] = 'config.production'
 import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
-bind = '0.0.0.0:18000'
+bind = '0.0.0.0:80'
 proc_name = 'cc_gunicorn'
 pidfile = '/tmp/cc_gunicorn.pid'
 logfile = '/tmp/cc_gunicorn.log'
