bdc384aaf3741011622eb7c3a80bcde002b25f10
Nick Robinson <grobins2@gmail.com>
2013-07-22 22:20:16 -0400

    added gunicorn

---
+++ gunicorn.py
@@ -0,0 +1,13 @@
+import os
+os.environ['DIAG_CONFIG_MODULE'] = 'config.production'
+
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
+bind = '0.0.0.0:18000'
+proc_name = 'cc_gunicorn'
+pidfile = '/tmp/cc_gunicorn.pid'
+logfile = '/tmp/cc_gunicorn.log'
+worker_class = 'sync'
+debug = True
+daemon = True
