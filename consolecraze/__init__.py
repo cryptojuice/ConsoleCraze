632e45151a2a1471c517802fd3b8c843bf604361
Nick Robinson <grobins2@gmail.com>
2013-07-24 19:33:56 -0400

    blueprint for comment module'

--- consolecraze/__init__.py
+++ consolecraze/__init__.py
@@ -13,12 +13,16 @@ login_manager.init_app(app)
 def not_found(error):
     return render_template('404.html'), 404
 
+
 # Register application blueprints
 from consolecraze.users.views import mod as usersModule
 app.register_blueprint(usersModule)
 
 from consolecraze.articles.views import mod as articlesModule
 app.register_blueprint(articlesModule)
+
+from consolecraze.comments.views import mod as commentsModule
+app.register_blueprint(commentsModule)
 
 from consolecraze.frontend.views import mod as frontendModule
 app.register_blueprint(frontendModule)
