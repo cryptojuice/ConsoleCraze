8d982dec765f1d8be88449881b5e378027a510dc
Nick Robinson <grobins2@gmail.com>
2013-07-24 19:36:59 -0400

    error with scoped_session after adding user,article,comment relationships. Reverting to sessionmaker() session

--- consolecraze/database.py
+++ consolecraze/database.py
@@ -5,13 +5,16 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode = True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-    autoflush=False,
-    bind=engine))
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'],\
+        convert_unicode=True, echo=True)
+#db_session = scoped_session(sessionmaker(autocommit=False,\
+#    autoflush=False,
+#    bind=engine))
+Session = sessionmaker(bind=engine)
+db_session = Session()
 
 Base = declarative_base()
-Base.query = db_session.query_property()
+#Base.query = db_session.query_property()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
