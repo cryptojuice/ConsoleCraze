544a69a0165ae6cedff89d1425a88a2038b282ae
Nick Robinson <grobins2@gmail.com>
2013-07-29 22:38:35 -0400

    Seperated votes into it's own model

--- consolecraze/articles/models.py
+++ consolecraze/articles/models.py
@@ -15,11 +15,9 @@ class Article(Base):
     image_url = Column(String(200))
     user_id = Column(Integer, ForeignKey('users.id'))
     comments = relationship('Comment', backref='article') 
-    upvotes = Column(Integer)
-    downvotes = Column(Integer)
+    votes = relationship('Vote', backref='article')
 
-    def __init__(self, title, content, url,
-            user_id, image_url=None, upvotes=0, downvotes=0):
+    def __init__(self, title, content, url, user_id, image_url=None):
 
         # Note eventually add default image_url to import
 
@@ -28,8 +26,6 @@ class Article(Base):
         self.url = url
         self.user_id = user_id
         self.image_url = image_url
-        self.upvotes = upvotes
-        self.downvotes = downvotes
 
     def __repr__(self):
         return "user_id(%s) submitted Article.title(%s)" % (self.user_id, self.title)
